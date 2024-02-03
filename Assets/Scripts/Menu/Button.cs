640007cd20f63a19a9ac2a6137d76c2396a5b9e5
GICF <ganggang2011@icloud.com>
2024-02-03 17:13:20 +0800

    优化按钮交互和UI

--- Assets/Scripts/Menu/Button.cs
+++ Assets/Scripts/Menu/Button.cs
@@ -7,8 +7,6 @@ public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     // 文本组件
     private Text text;
-    // 按钮的图片组件
-    private Image buttonImage;
     // 音频源
     private AudioSource audioSource;
     [Tooltip("鼠标悬停在按钮上时发出的声音")] public AudioClip choosAudio;
@@ -18,20 +16,16 @@ public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         // 获取组件
         text = GetComponentInChildren<Text>();
-        buttonImage = GetComponent<Image>();
         audioSource = GetComponent<AudioSource>();
         // 初始化颜色
-        buttonImage.color = Color.clear;
-        text.color = Color.white;
+        text.color = new Color(0.75f,0.75f,0.75f,255);
     }
 
     // 当鼠标进入按钮区域时调用
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // 渐变更改按钮颜色
-        StartCoroutine(ChangeColor(buttonImage, Color.white, 0.2f));
         // 渐变更换文本颜色
-        StartCoroutine(ChangeColor(text, Color.black, 0.2f));
+        StartCoroutine(ChangeColor(text, Color.white, 0.2f));
         // 播放选择声音
         audioSource.clip = choosAudio;
         audioSource.Play();
@@ -40,10 +34,8 @@ public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     // 当鼠标离开按钮区域时调用
     public void OnPointerExit(PointerEventData eventData)
     {
-        // 渐变恢复按钮的默认颜色
-        StartCoroutine(ChangeColor(buttonImage, Color.clear, 0.2f));
         // 渐变更换文本颜色
-        StartCoroutine(ChangeColor(text, Color.white, 0.2f));
+        StartCoroutine(ChangeColor(text, new Color(0.75f,0.75f,0.75f,255), 0.2f));
     }
 
     /// <summary>
